8963684baae590cff30cc4719960e73b799cfb86
RahulKrishna15 <rahulkrishnapooboni057@gmail.com>
2024-10-03 04:35:17 +0530

    palindrome.js Solution added

--- week-2/week-2-js/medium/palindrome.js
+++ week-2/week-2-js/medium/palindrome.js
@@ -4,7 +4,16 @@
 */
 
 function isPalindrome(str) {
-  return true;
+  str = str.replace(/[^a-z0-9]/gi, "")
+  str = str.toLowerCase().trim();
+  let str1 = str.split(""); // Split the string into an array of characters
+  let str2 = [...str1].reverse(); // Create a reversed version of the array
+  for (let i = 0; i < str1.length; i++) {
+    if (str1[i] !== str2[i]) {
+      return false; // If a mismatch is found, it's not a palindrome
+    }
+  }
+  return true; // If no mismatches are found, it is a palindrome
 }
 
 module.exports = isPalindrome;
